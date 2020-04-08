fa07d946ee178d69d2678044b5be629390c6c688
cliffordten <tenengclifford5@gmail.com>
2020-04-08 13:06:04 +0100

    code style fixed :(

--- src/estimator.js
+++ src/estimator.js
@@ -13,7 +13,8 @@ const currentInfections = (data) => {
   const currentlyInfected = data.reportedCases * 10;
   const infectionsByRequestedTime = currentlyInfected * calculatePeriod(data);
   const severeCasesByRequestedTime = (15 / 100) * infectionsByRequestedTime;
-  const hospitalBedsByRequestedTime = data.totalHospitalBeds - (35 / 100) * severeCasesByRequestedTime;
+  const compute = (35 / 100) * severeCasesByRequestedTime;
+  const hospitalBedsByRequestedTime = data.totalHospitalBeds - compute;
 
   return {
     currentlyInfected,
@@ -26,7 +27,8 @@ const projectedInfections = (data) => {
   const currentlyInfected = data.reportedCases * 50;
   const infectionsByRequestedTime = currentlyInfected * calculatePeriod(data);
   const severeCasesByRequestedTime = (15 / 100) * infectionsByRequestedTime;
-  const hospitalBedsByRequestedTime = data.totalHospitalBeds - (35 / 100) * severeCasesByRequestedTime;
+  const compute = (35 / 100) * severeCasesByRequestedTime;
+  const hospitalBedsByRequestedTime = data.totalHospitalBeds - compute;
 
   return {
     currentlyInfected,
